705838b57ae227ccb54d98cf345a7724881d67b9
Kiran M P <93343512+Kiran-M-P@users.noreply.github.com>
2022-01-06 22:33:13 +0530

    Add files via upload

---
+++ 02Variables.js
@@ -0,0 +1,11 @@
+// unmutale variable
+const myname1 = "kiran";
+
+// mutable variable
+let myname2 = "kumar";
+myname2 = "kutty";
+
+// mutable variable, depcricated
+var myname3 = "eshwar";
+
+console.log(myname1, myname2, myname3);
---
+++ 03DataTypes.js
@@ -0,0 +1,28 @@
+// primitive data types all immutable
+// 01 string ""
+const myname = "kiran"
+
+// 02 number
+let myage = 20
+
+// 04 undefined = value of a var not being initialized
+let unknown
+
+// 05 bigint 
+const bignumber = 111111111111111n
+
+// 06 null = value not exist primitive ull is not as "primitive" as it first seems!
+// Every Object is derived from null value, and therefore typeof operator returns object for it
+let zero = null
+
+
+// array ~ list python. usually group of same datatypes , also dif datatypes
+let myfriends = ["mk", "haribhai", "baji"]
+
+// typeof
+
+const datatype = (typeof(bignumber))
+console.log(datatype)
+
+
+
